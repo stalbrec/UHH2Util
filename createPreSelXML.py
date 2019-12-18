727d3140c812b12498df25a52e68e341b1397295
Steffen <steffen.albrecht@desy.de>
2019-12-18 11:34:33 +0100

    fixed createPreSelXML.py

--- createPreSelXML.py
+++ createPreSelXML.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from __future__ import print_function
 import os,glob,sys
+sys.path.append('/afs/desy.de/user/a/albrechs/xxl/af-cms/UHH2/10_2/CMSSW_10_2_10/src/UHH2/scripts/crab')
+from readaMCatNloEntries import *
+
 snippet='<In FileName="%s" Lumi="0.0"/>\n'
 if(len(sys.argv)<2):
-    print('Please provide config XML!')
     exit(0)
-
-print(sys.argv[1])
-inXML=open(sys.argv[1],'r+', encoding="utf-8")
-dir=''
-
-for l in inXML:
-    if('OUTdir' in l or 'OutputDirectory' in l):
-        dir = l.split('"')[1]
-        break
+inXML_path=os.path.abspath(sys.argv[1])
+OutDir=inXML_path.replace(inXML_path.split('/')[-1],'PreSel/')
+if(not os.path.exists(OutDir)):
+    os.mkdir(OutDir)
+inXML=open(inXML_path,'r+')
+root_dir = ''
+workdir = ''
 datasets=[]
 for l in inXML:
-    if('workdir' in l):
-        dir+=l.split('"')[9]
+    if('ENTITY' in l and 'OUTdir' in l):
+        root_dir_var = l.split('"')[1]
+    if('OutputDirectory' in l and '<!-' not in l):
+        root_dir = l.split('"')[3]
+    if('workdir' in l and 'ConfigSGE' in l):
+        workdir = l.split('"')[9]
     if('<InputData Version=' in l and '<!--' not in l):
         datasets.append(l.split('"')[1])
+print('root_dir_var',root_dir_var)
+if(root_dir_var):
+    root_dir = root_dir_var
+root_dir+=workdir
+print('root_dir',root_dir)
 inXML.close()
-print(dir)
-print(datasets)
-
 for dataset in datasets:
-    rule=dir+'/*'+dataset+'*'
+    rule=root_dir+'/*'+dataset+'*'
     print(rule)
     files=glob.glob(rule)
-    outXML=open('PreSel/'+dataset+'.xml','w')
+    outXML=open(OutDir+dataset+'.xml','w')
     for file in files:
         outXML.write(snippet%file)
     outXML.close()
+    readEntries(4,[OutDir+dataset+'.xml'],True)
