b6154f0725e620236d5d1d177dd2647eb256b279
Steffen Albrecht <steffen.albrecht@desy.de>
2020-02-10 10:05:38 +0100

    createPreSelXML.py now uses the correct readMCatNloEntries script (not the one from SFrameBatch)

--- createPreSelXML.py
+++ createPreSelXML.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import os,glob,sys
+for i,p in enumerate(sys.path):
+    if('sframebatch' in p.lower()):
+        sys.path.pop(i)
 sys.path.append('/afs/desy.de/user/a/albrechs/xxl/af-cms/UHH2/10_2/CMSSW_10_2_10/src/UHH2/scripts/crab')
 from readaMCatNloEntries import *
 
